da79fe02c29e7010513c4094466d6596bb943232
Muhammad Alif Ramsya <aliframsya16_github@example.com>
2026-06-23 17:33:50 +0700

    Ganti Menu

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -11,12 +11,13 @@ class MenuSeeder extends Seeder
     public function run(): void
     {
         $daftarMenu = [
-            [ 'nama' => 'Nasi Goreng Spesial'],
-            [ 'nama' => 'Mie Ayam Jamur'],
-            [ 'nama' => 'Sate Ayam Madura'],
-            [ 'nama' => 'Gado-Gado Surabaya'],
-            [ 'nama' => 'Rendang Daging Sapi'],
-            [ 'nama' => 'Ayam Bakar Taliwang'],
+            [ 'nama' => 'Sayur sawi dan kol, ayam pedas manis, buah kelengkeng, susu Diamond Milk, dan nasi putih '],
+            [ 'nama' => 'Nasi, jeruk, tempe goreng tepung, bayam bening, labu siam, jagung manis, dan ayam bumbu bistik'],
+            [ 'nama' => 'Mie kecap, asam semur, sawi rebus, yakult, pisang, and tempe goreng 
+'],
+            [ 'nama' => 'Nasi putih, ayam semur, tumis toge, wortel, jagung, tahu goreng crispy, dan semangka kuning'],
+            [ 'nama' => 'Nasi putih, ayam teriyaki gurih manis, tempe goreng renyah, tumis bayam & wortel, dan buah anggur'],
+            [ 'nama' => 'nasi putih, ayam semur, tumis toge, wortel, jagung, tahu goreng crispy dan semangka kuning'],
         ];
 
         $semuaDapur = Kitchen::all();
